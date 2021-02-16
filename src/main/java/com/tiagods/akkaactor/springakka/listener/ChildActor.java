8678c96fc6e461bfd759596dfdef8eac391c28e1
tiagods <tiagoice@hotmail.com>
2021-02-16 18:30:42 -0300

    Add SupervisorStrategy

--- src/main/java/com/tiagods/akkaactor/springakka/listener/ChildActor.java
+++ src/main/java/com/tiagods/akkaactor/springakka/listener/ChildActor.java
@@ -1,10 +1,24 @@
 package com.tiagods.akkaactor.springakka.listener;
 
+import akka.actor.OneForOneStrategy;
+import akka.actor.SupervisorStrategy;
 import akka.actor.UntypedActor;
+import akka.japi.Function;
 import lombok.extern.slf4j.Slf4j;
+import scala.concurrent.duration.Duration;
 
 @Slf4j
 public class ChildActor extends UntypedActor {
+
+    @Override
+    public SupervisorStrategy supervisorStrategy() {
+        return new OneForOneStrategy(-1, Duration.Inf(), new Function<Throwable, SupervisorStrategy.Directive>() {
+            @Override
+            public SupervisorStrategy.Directive apply(Throwable throwable) throws Exception {
+                return OneForOneStrategy.resume();
+            }
+        });
+    }
 
     @Override
     public void onReceive(Object msg) throws Exception {
